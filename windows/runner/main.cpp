76faa4172f6302cf9966500fad64b443b17970d5
LittleSheep <littlesheep.code@hotmail.com>
2025-05-24 17:35:50 +0800

    :bento: Update product name

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -30,7 +30,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"island", origin, size)) {
+  if (!window.Create(L"Solian", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
