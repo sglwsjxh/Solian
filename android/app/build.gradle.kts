8956723ac5f062214366ee5754f267e50329c750
LittleSheep <littlesheep.code@hotmail.com>
2025-08-06 03:30:33 +0800

    :rocket: Launch 3.1.0+117

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.google.firebase:firebase-messaging-ktx")
 }
 
 flutter {
