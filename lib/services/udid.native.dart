f7dad5e4195ed1e0c5d979697b0c68e7aecd5f61
LittleSheep <littlesheep.code@hotmail.com>
2025-09-06 13:09:27 +0800

    :zap: Cache udid

--- lib/services/udid.native.dart
+++ lib/services/udid.native.dart
@@ -1,5 +1,11 @@
 import 'package:flutter_udid/flutter_udid.dart';
 
+String? _cachedUdid;
+
 Future<String> getUdid() async {
-  return await FlutterUdid.consistentUdid;
+  if (_cachedUdid != null) {
+    return _cachedUdid!;
+  }
+  _cachedUdid = await FlutterUdid.consistentUdid;
+  return _cachedUdid!;
 }
