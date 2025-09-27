4981a23e8e16f7f97598a9469662ab2ed5b196f0
LittleSheep <littlesheep.code@hotmail.com>
2025-09-27 17:07:19 +0800

    :sparkles: Chat summary realtime updates

--- lib/pods/chat_summary.dart
+++ lib/pods/chat_summary.dart
@@ -1,6 +1,7 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:island/models/chat.dart';
 import 'package:island/pods/network.dart';
+import 'package:island/pods/websocket.dart';
 
 part 'chat_summary.g.dart';
 
@@ -12,9 +13,27 @@ class ChatSummary extends _$ChatSummary {
     final resp = await client.get('/sphere/chat/summary');
 
     final Map<String, dynamic> data = resp.data;
-    return data.map(
+    final summaries = data.map(
       (key, value) => MapEntry(key, SnChatSummary.fromJson(value)),
     );
+
+    final ws = ref.watch(websocketProvider);
+    final subscription = ws.dataStream.listen((WebSocketPacket pkt) {
+      if (!pkt.type.startsWith('messages')) return;
+      if (pkt.type == 'messages.new') {
+        final message = SnChatMessage.fromJson(pkt.data!);
+        updateLastMessage(message.chatRoomId, message);
+      } else if (pkt.type == 'messages.update') {
+        final message = SnChatMessage.fromJson(pkt.data!);
+        updateMessageContent(message.chatRoomId, message);
+      }
+    });
+
+    ref.onDispose(() {
+      subscription.cancel();
+    });
+
+    return summaries;
   }
 
   Future<void> clearUnreadCount(String chatId) async {
@@ -56,6 +75,21 @@ class ChatSummary extends _$ChatSummary {
           chatId: SnChatSummary(
             unreadCount: summary.unreadCount + 1,
             lastMessage: summary.lastMessage,
+          ),
+        });
+      }
+    });
+  }
+
+  void updateMessageContent(String chatId, SnChatMessage message) {
+    state.whenData((summaries) {
+      final summary = summaries[chatId];
+      if (summary != null && summary.lastMessage?.id == message.id) {
+        state = AsyncData({
+          ...summaries,
+          chatId: SnChatSummary(
+            unreadCount: summary.unreadCount,
+            lastMessage: message,
           ),
         });
       }
