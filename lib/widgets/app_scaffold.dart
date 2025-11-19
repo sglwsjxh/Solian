faf3a677d4350b37ad9cf52c5d0a14b1434b4cf9
LittleSheep <littlesheep.code@hotmail.com>
2025-11-20 00:21:21 +0800

    :rewind: Rewind AI slop

--- lib/widgets/app_scaffold.dart
+++ lib/widgets/app_scaffold.dart
@@ -314,6 +314,9 @@ class AppScaffold extends HookConsumerWidget {
       return null;
     }, []);
 
+    final appBarHeight = appBar?.preferredSize.height ?? 0;
+    final safeTop = MediaQuery.of(context).padding.top;
+
     final noBackground = isNoBackground ?? isWideScreen(context);
 
     final builtWidget = Focus(
@@ -322,7 +325,16 @@ class AppScaffold extends HookConsumerWidget {
         extendBody: extendBody ?? true,
         extendBodyBehindAppBar: true,
         backgroundColor: Colors.transparent,
-        body: body,
+        body: Column(
+          children: [
+            IgnorePointer(
+              child: SizedBox(
+                height: appBar != null ? appBarHeight + safeTop : 0,
+              ),
+            ),
+            if (body != null) Expanded(child: body!),
+          ],
+        ),
         appBar: appBar,
         bottomNavigationBar: bottomNavigationBar,
         bottomSheet: bottomSheet,
