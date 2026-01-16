72f23a504e7f2b4886ef8cfb100a18a6c4f186bf
LittleSheep <littlesheep.code@hotmail.com>
2026-01-16 21:21:51 +0800

    :arrow_up: Upgrade dependecies
    :heavy_minus_sign: Remove some unused deps

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -33,7 +33,6 @@ import media_kit_libs_macos_video
 import media_kit_video
 import package_info_plus
 import pasteboard
-import path_provider_foundation
 import protocol_handler_macos
 import record_macos
 import screen_retriever_macos
@@ -78,7 +77,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PasteboardPlugin.register(with: registry.registrar(forPlugin: "PasteboardPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ProtocolHandlerMacosPlugin.register(with: registry.registrar(forPlugin: "ProtocolHandlerMacosPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
