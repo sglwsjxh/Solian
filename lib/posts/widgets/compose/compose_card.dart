12423133f2f2d07d6bcef4190333d051276174b6
LittleSheep <hi@littlesheep.me>
2026-05-07 00:42:27 +0800

    :bug: Fix the compose sidebar enter don't auto close

--- lib/posts/widgets/compose/compose_card.dart
+++ lib/posts/widgets/compose/compose_card.dart
@@ -2,6 +2,7 @@ import 'dart:math' as math;
 
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -257,15 +258,34 @@ class PostComposeCard extends HookConsumerWidget {
             Expanded(
               child: KeyboardListener(
                 focusNode: FocusNode(),
-                onKeyEvent: (event) => ComposeLogic.handleKeyPress(
-                  event,
-                  composeState,
-                  ref,
-                  context,
-                  originalPost: originalPost,
-                  repliedPost: repliedPost,
-                  forwardedPost: forwardedPost,
-                ),
+                onKeyEvent: (event) {
+                  if (event is! KeyDownEvent) {
+                    return;
+                  }
+
+                  final isModifierPressed =
+                      HardwareKeyboard.instance.isMetaPressed ||
+                      HardwareKeyboard.instance.isControlPressed;
+                  final isSubmit =
+                      event.logicalKey == LogicalKeyboardKey.enter;
+
+                  if (isSubmit &&
+                      isModifierPressed &&
+                      !composeState.submitting.value) {
+                    performSubmit();
+                    return;
+                  }
+
+                  ComposeLogic.handleKeyPress(
+                    event,
+                    composeState,
+                    ref,
+                    context,
+                    originalPost: originalPost,
+                    repliedPost: repliedPost,
+                    forwardedPost: forwardedPost,
+                  );
+                },
                 child: SingleChildScrollView(
                   padding: const EdgeInsets.all(16),
                   child: ConstrainedBox(
