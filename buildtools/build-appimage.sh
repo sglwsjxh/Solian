bd48a7b8203ef5525912282326ab91f8033765eb
LittleSheep <hi@littlesheep.me>
2026-05-14 01:45:38 +0800

    :bug: Put back the icon-padded.png to prevent the linux compile issue

--- buildtools/build-appimage.sh
+++ buildtools/build-appimage.sh
@@ -2,7 +2,7 @@ rm -rf Solian.AppDir
 mkdir Solian.AppDir
 cp -r build/linux/x64/release/bundle/* Solian.AppDir
 cp -r buildtools/appimage_config/* Solian.AppDir
-cp assets/icons/icon-padded.webp Solian.AppDir
+cp assets/icons/icon-padded.png Solian.AppDir
 sudo chmod +x buildtools/appimagetool-x86_64.AppImage
 sudo chmod +x Solian.AppDir/AppRun
 ./buildtools/appimagetool-x86_64.AppImage Solian.AppDir
